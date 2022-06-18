a1eb9a36f8159f6bd364cc23b6a5e5dd23ed21e6
ducchinh02 <leducchinh02@gmail.com>
2022-06-18 19:45:17 +0700

    fix bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,18 +7,14 @@ const backToTop = document.querySelector('.back-to-top')
 
 // scroll event
 window.addEventListener('scroll', () => {
-        if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-            navigation.classList.add("active")
-        } else {
-            navigation.classList.remove("active")
-        }
-        if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
-            backToTop.classList.add("active")
-        } else {
-            backToTop.classList.remove("active")
-        }
-    })
-    // back to top function
+    document.body.scrollTop > 150 || document.documentElement.scrollTop > 150 ? navigation.classList.add("active") : navigation.classList.remove("active");
+
+    document.body.scrollTop > 400 || document.documentElement.scrollTop > 400 ? backToTop.classList.add("active") : backToTop.classList.remove("active");
+
+})
+
+
+// back to top function
 const scrollTop = () => {
     if (document.body.scrollTop != 0 || document.documentElement.scrollTop != 0) {
         window.scrollBy(0, -50);
@@ -41,25 +37,25 @@ const closeMenu = () => {
 
 closeBtn.addEventListener('click', () => {
     closeMenu()
-})
+});
 overlay.addEventListener('click', () => {
     closeMenu()
-})
+});
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
     origin: 'top',
     distance: '40px',
     duration: 1500
-})
+});
 
 
-sr.reveal(`.header-main`)
-sr.reveal(`.brand`, { delay: 500 })
-sr.reveal(`.services`, { delay: 500 })
-sr.reveal(`.top-marketing`, { delay: 500 })
-sr.reveal(`.goal`, { delay: 500 })
-sr.reveal(`.feedback`, { delay: 500 })
-sr.reveal(`.blog`, { delay: 500 })
-sr.reveal(`.subscribes`, { delay: 500 })
-sr.reveal(`.footer`, { delay: 500 })
+sr.reveal(`.header-main`);
+sr.reveal(`.brand`, { delay: 500 });
+sr.reveal(`.services`, { delay: 500 });
+sr.reveal(`.top-marketing`, { delay: 500 });
+sr.reveal(`.goal`, { delay: 500 });
+sr.reveal(`.feedback`, { delay: 500 });
+sr.reveal(`.blog`, { delay: 500 });
+sr.reveal(`.subscribes`, { delay: 500 });
+sr.reveal(`.footer`, { delay: 500 });
